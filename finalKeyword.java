d858af7f15b671a90b1e44fcd7e815e958ee0996
Khurram Shehzad <khurramshehzad2001@outlook.com>
2026-09-21 19:16:16 +0500

    adding pillars of oop

---
+++ finalKeyword.java
@@ -0,0 +1,11 @@
+public class finalKeyword {
+    /*
+    final keyword is used to make a variable constant it can be a method or can be a class or any keyword
+     */
+    final static int PI = 23;
+    public static void main(String[] args) {
+        final int pi = finalKeyword.PI;
+        System.out.println(pi);
+
+    }
+}
---
+++ pillarsOfOOP.java
@@ -0,0 +1,9 @@
+public class pillarsOfOOP {
+    /*
+    following are the pillars of the OOP we
+    1--Encapsulation
+    2--inheritance
+    3--polymorphism
+    4--abstraction
+     */
+}
