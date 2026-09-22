be97cda6a1162c1dba359bbe8bc5ee9e5a43ccbd
Khurram Shehzad <khurramshehzad2001@outlook.com>
2026-09-22 20:25:57 +0500

    adding abstraction concepts and code

--- staticClass.java
+++ staticClass.java
@@ -1,6 +1,7 @@
 public class staticClass {
     /*
     we made static class as a nested class so that we dont need to make object of the outer class to access innner class if it is static
+    a static class can inherits another static class
      */
     static class inner{
         public static void display(){
